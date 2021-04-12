0442f211435b895a04227b1453b1babccafe1607
swatiM19 <swati.munda@gmail.com>
2021-04-12 20:16:23 +0530

    need to check for polygon

--- outlet-server/controllers/Outlet.js
+++ outlet-server/controllers/Outlet.js
@@ -4,17 +4,7 @@ let path = require('path');
 
 function isCoordInPolygon (latitude, longitude, polygon) {
     const x = latitude; const y = longitude
-    let inside = false
-    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-        const xi = polygon[i][0];
-        const yi = polygon[i][1];
-        const xj = polygon[j][0];
-        const yj = polygon[j][1];
-        const val = ((yi > y) !== (yj > y)) &&
-            (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
-        if (val) inside = !inside
-    }
-    return inside
+    // check here if it is inside polygon or not.
 }
 
 let ctrl = {
@@ -40,6 +30,7 @@ let ctrl = {
                         // Could not complete it because of lack of time.
                     }
                 }
+                res.json({ message: "Not Found" });
 
             });
 
